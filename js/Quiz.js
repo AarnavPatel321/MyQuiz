4bc2bd320a19916dee13eae350b8695c75136a41
AarnavPatel321 <73790531+AarnavPatel321@users.noreply.github.com>
2021-06-11 09:05:18 -0500

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,58 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    question.hide();
+    background("Orange");
+    fill ("White");
+    textSize(30);
+    text("Result of the Quiz",340,50);
+    text("----------------------",320,65);
+    Contestant.getPlayerInfo();
+    if(allContestants !== undefined){
+      debugger;
+      var display_answer=230;
+      fill("blue");
+      textSize(20);
+      text("Note: contestant who answered correct are highlighted in green color!",130,230);
+      for(var plr in allContestants){
+        debugger;
+        var correctAnswer="2";
+        if(correctAnswer===allContestants[plr].answer)
+        fill("green");
+        else 
+        fill("red");
+        display_answer+=30;
+        textSize(20);
+        text(allContestants[plr].name+" : "+allContestants[plr].answer,250,display_answer)
+      }
+    } 
+  }
+}
